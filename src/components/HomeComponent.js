6f81d77ca688f3316279eaecfb2c62c2f591b762
TheRoro <39977138+TheRoro@users.noreply.github.com>
2020-08-16 14:17:38 -0500

    Initial commit

---
+++ src/components/HomeComponent.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import MediaPlayer from './MediaPlayer';
+
+function Home() {
+    return(
+        <div className="container home">
+            <div className="row justify-content-center">
+                <div className="col-auto mt-4">
+                    <h1>My Tones</h1>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Home;
